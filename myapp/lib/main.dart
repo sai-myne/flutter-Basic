fd21e11976a84698895247c57afba9b9b3a919fd
sai-myne <ahlusir@gmail.com>
2020-07-24 12:07:37 +0800

    Assets

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -14,16 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello ninjas!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: Image.asset('assets/space1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
