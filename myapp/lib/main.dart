230487211cb22819178106842e7a984bcd42dce1
sai-myne <ahlusir@gmail.com>
2020-07-24 10:46:50 +0800

    Colours-

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('hello ninjas'),
+      child: Text(
+        'hello ninjas!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
       child: Text('click'),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
